b79ddd8c21a460a9eb3390584f7c328fa1bfb42c
David M. Fajardo <davidmf99@gmail.com>
2021-12-26 17:46:39 +0100

    compare players page

--- pages/compare.tsx
+++ pages/compare.tsx
@@ -1,9 +1,8 @@
-import React, { useEffect, useState } from 'react'
+import React, { useState } from 'react'
 import axios from 'axios'
 import { backend, players } from '../config'
-import { ChartCard, ProgressByPlayer } from '../components'
-import { getStatValues, trophyIcon, statTitle, Rank } from '../utils'
-import { Chart, Player, RankResults } from '../interfaces/interfaces'
+import { statTitle, Rank, getStats } from '../utils'
+import { Player, PlayerStatsResult } from '../interfaces/interfaces'
 import { styles } from '../styles/styles.config'
 
 // ┌────────────────┐
@@ -15,6 +14,7 @@ export default function Compare(props: { data: Player[] }) {
     const [left, setLeft] = useState(0)
     const [right, setRight] = useState(0)
 
+    // logic when a player card is selected
     const handleSelect = (idx: number) => {
         if (!left) setLeft(idx + 1)
         else if (!right) setRight(idx + 1)
@@ -24,57 +24,103 @@ export default function Compare(props: { data: Player[] }) {
         }
     }
 
+    // styles
     const playerSelected = (idx: number) => {
-        if (idx + 1 === left) return 'bg-blue-400'
-        if (idx + 1 === right) return 'bg-red-400'
-        return ''
+        if (idx + 1 === left) return 'bg-blue-400' // left
+        else if (idx + 1 === right) return 'bg-red-400' // right
+        return 'bg-zinc-100 dark:bg-zinc-800' // unselected
     }
 
-    const progressBar = (percent: number, title: string, reverse: boolean) => (
-        <>
-            <span>{title}</span>
-            <div className={`bg-zinc-200 dark:bg-zinc-700 h-3 rounded flex ${reverse ? 'justify-end' : ''}`}>
-                <div className='bg-teal-500 h-3 rounded' style={{ width: `${percent}%` }}></div>
+    const rankStructure = (player: Player) => (
+        <div className='flex items-center justify-center'>
+            <div className='relative flex flex-col items-center text-sm text-white'>
+                <img className='m-3 w-14 rounded' src={player.image} alt={player.name} />
+                <span className='px-1 absolute bottom-0 bg-zinc-700 border border-yellow-500 rounded-full'>{player.level}</span>
             </div>
-        </>
+            <div className='flex flex-col'>
+                <h2 className='text-xl'>{player.name}</h2>
+                <h3>({player.alias})</h3>
+            </div>
+            <div className='m-2'>
+                <Rank
+                    title={'Solo/Duo'}
+                    rank={player.rank.solo.rank}
+                    image={player.rank.solo.image}
+                    lp={player.rank.solo.lp}
+                    win={player.rank.solo.win}
+                    lose={player.rank.solo.lose}
+                    winrate={player.rank.solo.winrate}
+                />
+            </div>
+            <div className='m-2'>
+                <Rank
+                    title={'Flex'}
+                    rank={player.rank.flex.rank}
+                    image={player.rank.flex.image}
+                    lp={player.rank.flex.lp}
+                    win={player.rank.flex.win}
+                    lose={player.rank.flex.lose}
+                    winrate={player.rank.flex.winrate}
+                />
+            </div>
+        </div>
     )
 
-    const playerData = (player: Player, reverse: boolean) => (
-        <div>
-            <div className='flex items-center justify-center'>
-                <div className='relative flex flex-col items-center text-sm text-white'>
-                    <img className='m-3 w-14 rounded' src={player.image} alt={player.name} />
-                    <span className='px-1 absolute bottom-0 bg-zinc-700 border border-yellow-500 rounded-full'>{player.level}</span>
-                </div>
-                <div className='flex flex-col'>
-                    <h2 className='text-xl'>{player.name}</h2>
-                    <h3>({player.alias})</h3>
-                </div>
-                <div className='m-2'>
-                    <Rank
-                        title={'Solo/Duo'}
-                        rank={player.rank.solo.rank}
-                        image={player.rank.solo.image}
-                        lp={player.rank.solo.lp}
-                        win={player.rank.solo.win}
-                        lose={player.rank.solo.lose}
-                        winrate={player.rank.solo.winrate}
-                    />
-                </div>
-                <div className='m-2'>
-                    <Rank
-                        title={'Flex'}
-                        rank={player.rank.flex.rank}
-                        image={player.rank.flex.image}
-                        lp={player.rank.flex.lp}
-                        win={player.rank.flex.win}
-                        lose={player.rank.flex.lose}
-                        winrate={player.rank.flex.winrate}
-                    />
+    // TODO: get dinamically this
+    const stats: string[] = [
+        'games',
+        'winrate',
+        'kda',
+        'kills',
+        'deaths',
+        'assists',
+        'cs',
+        'csmin',
+        'gold',
+        'max_kills',
+        'max_deaths',
+        'avg_damage_dealt',
+        'avg_damage_taken',
+        'double_kills',
+        'triple_kills',
+        'quadra_kills',
+        'penta_kills',
+    ]
+
+    const progressBar = (l_value: number, r_value: number, title: string, activated: boolean) => {
+        const calcWidth = (x: number, y: number) => (100 * x) / (x + y)
+
+        return (
+            <div className='flex justify-end items-end m-1'>
+                <span className='mx-2'>{statTitle(title)}</span>
+                <div className='w-96'>
+                    <div className='relative h-px text-center text-white'>
+                        <div className='absolute left-2 top-0 text-sm'>{l_value}</div>
+                        <div className='-translate-y-2 text-2xl'>|</div>
+                        <div className='absolute right-2 top-0 text-sm'>{r_value}</div>
+                    </div>
+                    {activated ? (
+                        <div className='bg-red-400 dark:bg-red-400/75 rounded h-5'>
+                            <div
+                                className='bg-blue-500 dark:bg-blue-500/75 rounded-tl rounded-bl h-5'
+                                style={{ width: `${calcWidth(l_value, r_value)}%` }}
+                            ></div>
+                        </div>
+                    ) : (
+                        <div className='bg-zinc-400 dark:bg-zinc-600 rounded h-5'></div>
+                    )}
                 </div>
             </div>
-            {progressBar(100 - player.rank.rank_p, 'Global rank', reverse)}
-            {progressBar(100 - player.rank.rank_p, 'Global rank', reverse)}
+        )
+    }
+
+    const comparePlayers = (leftPlayer: PlayerStatsResult, rightPlayer: PlayerStatsResult) => (
+        <div className='container flex flex-col justify-center items-center mx-auto'>
+            <div>
+                {stats.map((stat, idx) => (
+                    <div key={idx}>{progressBar(leftPlayer[stat], rightPlayer[stat], stat, leftPlayer[stat] || rightPlayer[stat])}</div>
+                ))}
+            </div>
         </div>
     )
 
@@ -85,7 +131,7 @@ export default function Compare(props: { data: Player[] }) {
                     {props.data.map((player, idx) => (
                         <div
                             key={idx}
-                            className={`p-2 cursor-pointer ${styles.foreground} ${styles.card} ${playerSelected(idx)}`}
+                            className={`p-2 cursor-pointer ${styles.card} ${playerSelected(idx)}`}
                             onClick={() => handleSelect(idx)}
                         >
                             <div className='flex items-center justify-around'>
@@ -104,18 +150,43 @@ export default function Compare(props: { data: Player[] }) {
                     ))}
                 </div>
             </div>
-            <div className='container-fluid p-4 grid gap-4 grid-cols-2'>
-                <div className='bg-blue-200 p-4'>
-                    <p>LADO AZUL (LEFT)</p>
-                    <p>index: {left}</p>
-                    {left && playerData(props.data[left - 1], false)}
-                </div>
-                <div className='bg-red-200 p-4'>
-                    <p>LADO ROJO (RIGHT)</p>
-                    <p>index: {right}</p>
-                    {right && playerData(props.data[right - 1], true)}
-                </div>
+
+            <h2 className='text-center text-2xl m-4'>Compare players</h2>
+            <hr className='w-4/5 mx-auto' />
+
+            <div className='container mx-auto p-4 grid gap-4 md:grid-cols-2'>
+                {/* If at least 1 player selected ==> show 'clear' button */}
+                {left != 0 || right != 0 ? (
+                    <div className='md:col-span-2'>
+                        <button
+                            className='bg-red-500 rounded text-white shadow py-2 px-3 mx-auto'
+                            onClick={() => {
+                                setLeft(0)
+                                setRight(0)
+                            }}
+                        >
+                            <i className='bi bi-x-lg'></i> Clear selection
+                        </button>
+                    </div>
+                ) : (
+                    ''
+                )}
+                <div className='bg-blue-400/25 rounded shadow'>{left != 0 && rankStructure(props.data[left - 1])}</div>
+                <div className='bg-red-400/25 rounded shadow'>{right != 0 && rankStructure(props.data[right - 1])}</div>
             </div>
+
+            {/* If no players selected in any side ==> alert message explaining */}
+            {left === 0 && right === 0 && (
+                <div className='w-96 mx-auto'>
+                    <div className='bg-sky-100 bg-sky-400/50 border-l-4 border-sky-500 text-sky-800 dark:text-white p-4'>
+                        <p className='font-bold'>No players selected</p>
+                        <p>Select 2 players above to start comparing stats!</p>
+                    </div>
+                </div>
+            )}
+
+            {/* If 2 players selected ==> compare them! */}
+            {left != 0 && right != 0 && comparePlayers(getStats(props.data[left - 1]), getStats(props.data[right - 1]))}
         </div>
     )
 }
---
+++ utils/getStats.ts
@@ -0,0 +1,66 @@
+import { Player, PlayerStatsResult } from '../interfaces/interfaces'
+
+/*
+ *  Returns the stats calculated for that player based on its 7 champs stats
+ */
+export default function getStats(player: Player) {
+    const result: PlayerStatsResult = {
+        name: player.name,
+        games: 0,
+        winrate: 0,
+        kda: 0,
+        kills: 0,
+        deaths: 0,
+        assists: 0,
+        cs: 0,
+        csmin: 0,
+        gold: 0,
+        max_kills: 0,
+        max_deaths: 0,
+        avg_damage_dealt: 0,
+        avg_damage_taken: 0,
+        double_kills: 0,
+        triple_kills: 0,
+        quadra_kills: 0,
+        penta_kills: 0,
+    }
+
+    // TODO: get dinamically this
+    const stats: string[] = [
+        'winrate',
+        'kda',
+        'kills',
+        'deaths',
+        'assists',
+        'cs',
+        'csmin',
+        'gold',
+        'max_kills',
+        'max_deaths',
+        'avg_damage_dealt',
+        'avg_damage_taken',
+    ]
+
+    // Calculate the total games of that player
+    player.champs.map((champ, idx) => {
+        result.games += champ.games
+        result.double_kills += champ.double_kills
+        result.triple_kills += champ.triple_kills
+        result.quadra_kills += champ.quadra_kills
+        result.penta_kills += champ.penta_kills
+    })
+
+    stats.map((stat, idx) => {
+        let games = 0
+        let stat_value = 0
+
+        player.champs.map((champ, idx) => {
+            games += champ.games
+            stat_value += champ.games * champ[stat]
+        })
+
+        result[stat] = parseFloat((stat_value / games).toFixed(2))
+    })
+
+    return result
+}
