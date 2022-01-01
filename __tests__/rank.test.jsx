4a4bd307a15fa406d096498b38afe4fa3bf3f3e4
David M. Fajardo <davidmf99@gmail.com>
2022-01-02 00:10:22 +0100

    first test

---
+++ __tests__/rank.test.jsx
@@ -0,0 +1,15 @@
+import { render, screen } from '@testing-library/react'
+import statTitle from '../utils/statTitle'
+
+describe('[statTitle.ts]: converts a stat key in its Title (if listed inside the function)', () => {
+    // Listed key: for example, 'winrate'
+    test('Returns correctly "winrate" key', () => {
+        expect(statTitle('winrate')).toBe('Winrate')
+    })
+
+    // Non listed keys
+    test('Returns the same [string | number] if not listed as a stat', () => {
+        expect(statTitle('1234')).toBe('1234')
+        expect(statTitle(1234)).toBe(1234)
+    })
+})
