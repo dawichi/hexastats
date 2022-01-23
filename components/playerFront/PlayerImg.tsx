31783cb9ae5e6655438aca0efbfc846c0dd23c4f
Alex Paz <91157929+alexxwe@users.noreply.github.com>
2022-01-23 14:16:46 +0100

    Rounded Image

--- components/playerFront/PlayerImg.tsx
+++ components/playerFront/PlayerImg.tsx
@@ -2,7 +2,7 @@ import Image from 'next/image'
 
 const PlayerImg = ({ image, alias, level }: { image: string; alias: string; level: number }) => (
     <div className='relative flex flex-col items-center text-sm text-white m-3 w-14 h-14 rounded'>
-        <Image src={image} layout='fill' alt={alias} />
+        <Image className='rounded-lg' src={image} layout='fill' alt={alias}/>
         <span className='px-1 absolute -bottom-2 bg-zinc-700 border border-yellow-500 rounded-full'>{level}</span>
     </div>
 )
