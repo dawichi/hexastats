62e324d36cf7a5d75e48fea4ca67d761c26e6f3a
David M. Fajardo <davidmf99@gmail.com>
2022-01-31 21:29:12 +0100

    update buymeacoffee

--- components/layout/Footer.tsx
+++ components/layout/Footer.tsx
@@ -34,7 +34,7 @@ const Footer = () => (
             <div className='flex items-center justify-center my-5'>
                 <a target='_blank' href='https://www.buymeacoffee.com/dawichi' rel='noreferrer'>
 					<div className='w-64 h-24 relative'>
-						<Image layout='fill' src='https://img.buymeacoffee.com/button-api/?text=Buy me a coffee&emoji=&slug=dawichi&button_colour=6366f1&font_colour=ffffff&font_family=Cookie&outline_colour=000000&coffee_colour=FFDD00' alt='Coffee' />
+						<Image layout='fill' src='https://img.buymeacoffee.com/button-api/?text=Buy me a coffee&emoji=&slug=dawichi&button_colour=818cf8&font_colour=ffffff&font_family=Cookie&outline_colour=000000&coffee_colour=FFDD00' alt='Coffee' />
 					</div>
                 </a>
             </div>
