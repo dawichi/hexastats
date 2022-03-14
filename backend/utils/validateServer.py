ed7a9207b3074e6a4463589bb576bb406d7a9efb
David M. Fajardo <davidmf99@gmail.com>
2022-03-14 21:29:12 +0100

    npm init

--- backend/utils/validateServer.py
+++
@@ -1,8 +0,0 @@
-def validate_server(server):
-	servers = ['euw', 'lan', 'las', 'na','eune', 'tr', 'oce', 'ru', 'jp', 'br']
-	if server == 'kr':
-		return 'www'
-	elif server in servers:
-		return server
-	else:
-		return servers[0]
