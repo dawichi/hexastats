d9a3e6a083aeea61bcc2db61535865dda7162847
David M. Fajardo <davidmf99@gmail.com>
2022-03-15 10:02:41 +0100

    basic backend

---
+++ backend/.prettierrc.js
@@ -0,0 +1,13 @@
+module.exports = {
+    printWidth: 140,
+    tabWidth: 4,
+    useTabs: false,
+    semi: false,
+    singleQuote: true,
+    jsxSingleQuote: true,
+    quoteProps: 'as-needed',
+    trailingComma: 'all',
+    bracketSpacing: true,
+    bracketSameLine: false,
+    arrowParens: 'avoid',
+}
