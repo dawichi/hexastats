02e275a81d5b5b064301caf7ae14fd8d53ef86d3
David M. Fajardo <davidmf99@gmail.com>
2022-03-16 19:49:06 +0100

    prettier

--- frontend/pages/api/player.js
+++ frontend/pages/api/player.js
@@ -1,5 +1,5 @@
 export default function handler(req, res) {
-    res.statusCode = 200;
-    res.setHeader('Content-Type', 'application/json');
-    res.json({ name: 'John Doe' });
+    res.statusCode = 200
+    res.setHeader('Content-Type', 'application/json')
+    res.json({ name: 'John Doe' })
 }
