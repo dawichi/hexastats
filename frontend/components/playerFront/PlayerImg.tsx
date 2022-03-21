12acda0e9075d29a4e7a063e7d69f24573172183
David M. Fajardo <davidmf99@gmail.com>
2022-03-21 17:33:38 +0100

    [ BUGFIX ]: PlayerImg jsdocs

--- frontend/components/playerFront/PlayerImg.tsx
+++ frontend/components/playerFront/PlayerImg.tsx
@@ -1,6 +1,13 @@
 import Image from 'next/image'
 
-const PlayerImg = ({ image, alias, level }: { image: string; alias: string; level: number }) => (
+/**
+ * Displays the level over a image of a player
+ * @param image url to the image
+ * @param alias name of the player
+ * @param level level of the player
+ * @returns {JSX.Element} the HTML structure
+ */
+const PlayerImg = ({ image, alias = 'playerImg', level = 0 }: { image: string; alias?: string; level?: number }): JSX.Element => (
     <div className='relative flex flex-col items-center text-sm text-white m-3 w-14 h-14 rounded'>
         <Image className='rounded-lg' src={image} layout='fill' alt={alias} />
         <span className='px-1 absolute -bottom-2 bg-zinc-700 border border-yellow-500 rounded-full'>{level}</span>
