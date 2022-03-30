3a2c921c042db8a94b984ce2aae9c9fea1be417f
Brr1-99 <93494400+Brr1-99@users.noreply.github.com>
2022-03-30 18:59:42 +0200

    Reorganize configs folder

---
+++ frontend/configs/flask.ts
@@ -0,0 +1,3 @@
+export const backend = 'https://hexastats-flask.vercel.app/?players='
+
+export const servers = ['euw', 'lan', 'las', 'na', 'kr', 'eune', 'tr', 'oce', 'ru', 'jp', 'br']
--- frontend/configs/index.ts
+++ frontend/configs/index.ts
@@ -2,6 +2,6 @@
 export { navigation as navigation } from './navigation'
 
 // API related confs
-export { backend as backend } from './api'
-export { servers as servers } from './api'
-export { riot as riot } from './api'
+export { backend as backend } from './flask'
+export { servers as servers } from './flask'
+export { riot as riot } from './riot'
