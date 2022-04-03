b4c1cb0da5f9f6fb422aaf1ce49a8ac78a2dedfb
Brr1-99 <93494400+Brr1-99@users.noreply.github.com>
2022-04-03 18:43:17 +0200

    profile icon

--- frontend/pages/test2.tsx
+++ frontend/pages/test2.tsx
@@ -4,51 +4,20 @@ import { PlayerImg, RankStructure } from 'components'
 
 const Test = () => {
     const [playerData, setPlayerData] = useState<any>()
-    const [league, setLeague] = useState<any>([{},{}])
 
     useEffect(() => {
         // GET data from player
-        fetch('/api/summoner/Alexwwe')
+        fetch('https:/backend-hexastats.vercel.app/Brr1')
             .then(res => res.json())
             .then(data => setPlayerData(data))
     }, [])
 
-    useEffect(() => {
-        // GET champion masteries
-        if (playerData) {
-            fetch('/api/league/' + playerData.id)
-                .then(res => res.json())
-                .then(data => setLeague(data))
-        }
-    }, [playerData])
-
-    console.table(playerData)
-    console.log(league)
-
-    const Rank = (idx: number) => ({
-        rank: league[idx]?.tier ? `${league[idx]?.tier} ${league[idx]?.rank}` : 'Unranked',
-        image: league[idx]?.tier ? `/images/league-emblems/${league[idx]?.tier}.png` : '/images/league-emblems/Unranked.png',
-        lp: league[idx]?.leaguePoints ?? 0,
-        win: league[idx]?.wins ?? 0,
-        lose: league[idx]?.losses ?? 0,
-        winrate: league[idx]?.wins && league[idx]?.losses ?
-            (league[idx]?.wins/(league[idx]?.wins+league[idx]?.losses) * 100).toFixed(0) : 0,
-    })
-    const player = {
-        image: riot.utils.profileIconUrl(playerData?.profileIconId),
-        alias: playerData?.name,
-        level: playerData?.summonerLevel,
-        rank: {
-            solo: Rank(0),
-            flex: Rank(1),
-        }
-    }
-    console.log(player)
-
+    console.log(playerData)
+    
     return (
         <div className='container mx-auto'>
-            {playerData && <PlayerImg image={riot.utils.profileIconUrl(playerData.profileIconId)} alias={playerData.name} level={playerData.summonerLevel}/>}
-            {league && <RankStructure player={player}/>}
+            {playerData && <PlayerImg image={playerData.data.image} alias={playerData.data.name} level={playerData.data.summonerLevel}/>}
+            {playerData && <RankStructure player={playerData.data}/>}
         </div>
     )
 }
