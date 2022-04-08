c66f9a0ea00c5324b2a3ff1aa732c20c8f7a096d
David M. Fajardo <davidmf99@gmail.com>
2022-04-08 14:34:16 +0200

    url correction

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -3,11 +3,4 @@ module.exports = {
     images: {
         domains: ['opgg-static.akamaized.net', 'ddragon.leagueoflegends.com', 'img.buymeacoffee.com'],
     },
-    typescript: {
-      // !! WARN !!
-      // Dangerously allow production builds to successfully complete even if
-      // your project has type errors.
-      // !! WARN !!
-      ignoreBuildErrors: true,
-    },
 }
