4206824dfbc668089badbaf5def4e99f9f3d771f
David M. Fajardo <davidmf99@gmail.com>
2022-04-08 14:23:21 +0200

    fix

--- frontend/components/playerFront/PlayerImg.tsx
+++ frontend/components/playerFront/PlayerImg.tsx
@@ -5,7 +5,6 @@ import Image from 'next/image'
  * @param image url to the image
  * @param alias name of the player
  * @param level level of the player
- * @returns {JSX.Element} the HTML structure
  */
 const PlayerImg = ({ image, alias = 'playerImg', level = 0 }: { image: string; alias?: string; level?: number }): JSX.Element => (
     <div className='relative flex flex-col items-center text-sm text-white m-3 w-14 h-14 rounded'>
