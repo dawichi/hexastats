594355a13b377b5d85a163b785da643cec3e2564
Brr1-99 <93494400+Brr1-99@users.noreply.github.com>
2022-04-21 16:05:42 +0200

    Query params api

--- backend/.eslintrc.js
+++ backend/.eslintrc.js
@@ -30,5 +30,6 @@ module.exports = {
             { blankLine: 'always', prev: ['const', 'let', 'var'], next: '*' },
             { blankLine: 'any', prev: ['const', 'let', 'var'], next: ['const', 'let', 'var'] },
         ],
+        "object-shorthand": "warn",
     },
 }
