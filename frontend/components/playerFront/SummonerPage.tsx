3a8fd374bee0c3ffd9831464c4786d30a02809d4
David M. Fajardo <davidmf99@gmail.com>
2022-05-13 14:08:19 +0200

    [ FEATURE ]: Summoner Page

--- frontend/components/playerFront/SummonerPage.tsx
+++ frontend/components/playerFront/SummonerPage.tsx
@@ -5,10 +5,21 @@ export default function SummonerPage({ player }: { player: Player }) {
     const a = 5
 
     return (
-        <section className='flex'>
-            <header className='m-5'>
-                <RankStructure player={player} />
-            </header>
-        </section>
+        <div className='relative' style={{
+            backgroundImage: `url(http://ddragon.leagueoflegends.com/cdn/img/champion/splash/${player.masteries[0].name}_0.jpg)`,
+            backgroundSize: 'cover',
+            backgroundPosition: 'center',
+        }}>
+            <section className='bg-orange-50/80 dark:bg-zinc-900/80 flex opacity-100'>
+                <header className='m-5'>
+                    <RankStructure player={player} />
+                    <RankStructure player={player} />
+                    <RankStructure player={player} />
+                    <RankStructure player={player} />
+                    <RankStructure player={player} />
+                </header>
+            </section>
+        </div>
     )
 }
+
--- frontend/configs/navigation.ts
+++ frontend/configs/navigation.ts
@@ -3,6 +3,10 @@ import { link } from 'interfaces/interfaces'
 // Navigation menu
 export const navigation: link[] = [
     {
+        name: 'Summoners',
+        url: '/summoners',
+    },
+    {
         name: 'Stats',
         url: '/stats',
     },
