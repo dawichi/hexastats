eb62b0ad13d3f7f55a6c2cca332e3a7c3476a29b
David M. Fajardo <davidmf99@gmail.com>
2022-05-14 23:38:53 +0200

    RiotService edit

--- frontend/services/RiotService.ts
+++ frontend/services/RiotService.ts
@@ -5,11 +5,34 @@ export class RiotService {
     private readonly baseUrl: string = 'http://ddragon.leagueoflegends.com/cdn/'
 
     /**
-     * ## Returns the URL to the champ image
+     * ## URL to the champ image
      * @param champName The name of the champ
      */
     champImage(champName: string) {
         return `https://opgg-static.akamaized.net/images/lol/champion/${champName}.png`
     }
 
+    /**
+     * ## URL to the champ splash art
+     * @param champName The name of the champ
+     */
+    champSplash(champName: string) {
+        return `https://ddragon.leagueoflegends.com/cdn/img/champion/splash/${champName}_0.jpg`
+    }
+
+    /**
+     * ## URL to the position image
+     * @param position The position of the champ
+     */
+    teamPositionIcon(position: string) {
+        const positions = {
+            TOP: 'position_top.png',
+            MID: 'position_mid.png',
+            JUNGLE: 'position_jungle.png',
+            BOTTOM: 'position_bottom.png',
+            SUPPORT: 'position_support.png'
+        }
+
+        return `https://raw.communitydragon.org/latest/plugins/rcp-fe-lol-career-stats/global/default/${positions[position] ?? positions.MID}`
+    }
 }
