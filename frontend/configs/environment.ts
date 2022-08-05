1816a995d29ff4a6d0703e750665dcd36ad22caa
David M. Fajardo <davidmf99@gmail.com>
2022-08-05 14:02:26 +0200

    Update environment.ts

--- frontend/configs/environment.ts
+++ frontend/configs/environment.ts
@@ -1,21 +1,13 @@
-const envConfig = {
-    /*
-    *  Local backend config
-    * 
-    * If you want to use a local backend (running the /backend/index.py flask server)
-    * just add a .env.local file with the following content:
-    *   LOCAL_API=True
-    */
-    local: process.env.LOCAL_API === 'true',
-    backendUrl: '',
+const development = process.env.LOCAL_API === 'true'
+
+/**
+ *  Backend config
+ * 
+ * If you want to use a local backend (running the `/backend` NestJS server)
+ * just add in .env.local file the following content:
+ *   
+ * LOCAL_API=true
+ */
+export const environment = {
+    backendUrl: development ? 'http://localhost:5000/' : 'https://api-hexastats.vercel.app/',
 }
-
-export const environment = new Proxy(envConfig, {
-    get: (target, key) => {
-        if (key === 'production') {
-            return target[key]
-        }
-
-        return envConfig.local ? 'http://localhost:5000/' : 'https://api-hexastats.vercel.app/'
-    }
-})
