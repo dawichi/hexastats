e9a8aebbf50c9643fa2cbdc7d222d0baaed68558
David M. Fajardo <davidmf99@gmail.com>
2022-08-21 22:32:27 +0200

    [ FIX ]: More compacted view GamesList

--- frontend/components/Summoner/GamesList.tsx
+++ frontend/components/Summoner/GamesList.tsx
@@ -19,11 +19,11 @@ const ItemsGrid = ({ items }: { items: { [key: number]: string } }) => (
                 return (
                     <span key={itemId}>
                         {items[itemId] ? (
-                            <Image className='rounded' src={items[itemId]} alt='item' width={40} height={40} />
+                            <Image className='rounded' src={items[itemId]} alt='item' width={36} height={36} />
                         ) : (
                             <div
                                 className='bg-gradient-to-br from-zinc-500 to-zinc-800 rounded'
-                                style={{ width: '40px', height: '40px' }}
+                                style={{ width: '36px', height: '36px' }}
                             />
                         )}
                     </span>
@@ -50,8 +50,8 @@ const SummonersGrid = ({ game }: { game: GameDto }) => {
                         className='rounded'
                         src={riotService.champImage(participant.champ.championName)}
                         alt='champion'
-                        width={30}
-                        height={30}
+                        width={24}
+                        height={24}
                     />
                     <span
                         className='ml-1 text-sm w-20 h-5 overflow-hidden'
@@ -104,7 +104,7 @@ export default function GamesList({ player }: { player: SummonerDto }) {
                         key={idx}
                         className={classNames(
                             `${styles.shadow} ${styles.background} ${styles.scale} ${win ? 'border-green-500' : 'border-red-500'}`,
-                            'cursor-pointer border-8 border-y-0 border-r-0 rounded-lg mx-4 my-2 h-26 grid grid-cols-3',
+                            'cursor-pointer border-8 border-y-0 border-r-0 rounded-lg mx-4 my-2 grid grid-cols-3',
                         )}
                     >
                         <div className='relative text-white'>
@@ -113,7 +113,7 @@ export default function GamesList({ player }: { player: SummonerDto }) {
                                 style={{
                                     backgroundImage: `url(${riotService.champSplash(championName)})`,
                                     backgroundSize: 'cover',
-                                    backgroundPosition: 'center',
+                                    backgroundPosition: 'top',
                                 }}
                             ></div>
                             <div
@@ -121,9 +121,9 @@ export default function GamesList({ player }: { player: SummonerDto }) {
                                 style={{ backgroundImage: 'linear-gradient(to top right, #000000bd , #ffffff00)' }}
                             ></div>
                             <div className='absolute top-2 left-3'>
-                                <Image src={riotService.teamPositionIcon(teamPosition)} alt='champ image' width={50} height={50} />
+                                <Image src={riotService.teamPositionIcon(teamPosition)} alt='champ image' width={40} height={40} />
                             </div>
-                            <span className='absolute top-4 left-16 text-xl text-center'>{champLevel}</span>
+                            <span className='absolute top-3 left-14 text-xl text-center'>{champLevel}</span>
                             <span className='absolute bottom-1 left-2'>{game.gameMode}</span>
                             <span className='absolute bottom-1 right-2'>
                                 {gameMinutes}:{gameSeconds}
@@ -132,15 +132,16 @@ export default function GamesList({ player }: { player: SummonerDto }) {
                         <div className='relative flex flex-col items-center text-center'>
                             <div className='absolute top-1 bottom-1 left-3'>
                                 <div className='flex flex-col h-full justify-around'>
-                                    <Image className='rounded' src={spells[1]} alt='spell 2' width={40} height={40} />
-                                    <Image className='rounded' src={spells[0]} alt='spell 1' width={40} height={40} />
-                                    <Image className='rounded' src={items[6]} alt='guard' width={40} height={40} />
+                                    <Image className='rounded' src={spells[1]} alt='spell 2' width={32} height={32} />
+                                    <Image className='rounded' src={spells[0]} alt='spell 1' width={32} height={32} />
+                                    <Image className='rounded' src={items[6]} alt='guard' width={32} height={32} />
                                 </div>
                             </div>
-                            <p className='text-xl'>
-                                {kills} / {deaths} / {assists}
+                            <p>
+                                <span>{kills} / {deaths} / {assists}</span>
+                                <span className='text-sm ml-6'>{calc_kda} kda</span>
+                                
                             </p>
-                            <p className='text-sm'>{calc_kda} kda</p>
                             <div className='mt-4 ml-4'>
                                 <ItemsGrid items={items} />
                             </div>
@@ -164,7 +165,7 @@ export default function GamesList({ player }: { player: SummonerDto }) {
                         </div>
                     ):(
                         'Load more'
-                )}
+                    )}
                 </span>
             </div>
         </>
