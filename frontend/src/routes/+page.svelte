ff63efb8db0cfc3981e2d70e62f45560ff2f5cd2
David M. Fajardo <davidmf99@gmail.com>
2022-11-05 21:29:09 +0100

    Home Page

--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -1,35 +1,23 @@
 <script lang="ts">
-    import RankStructure from "$lib/components/common/RankStructure.svelte"
-    import type { SummonerDto } from "$lib/types"
-
-    const player: SummonerDto = {
-        alias: "Sylver",
-        image: "https://ddragon.leagueoflegends.com/cdn/11.16.1/img/profileicon/28.png",
-        level: 30,
-        rank: {
-            solo: {
-                rank: "I",
-                lp: 0,
-                win: 0,
-                lose: 0,
-                winrate: 0,
-                image: "iron.png"
-            },
-            flex: {
-                rank: "I",
-                lp: 0,
-                win: 0,
-                lose: 0,
-                winrate: 0,
-                image: "iron.png"
-            }
-        },
-        games: [],
-        masteries: [],
-        server: 'wueq'
-    }
 </script>
 
-<div>
-    <RankStructure {player} />
+<div class='animate__animated animate__fadeIn min-h-screen container m-auto px-4 py-8 lg:py-16'>
+    <div class='text-center'>
+        <h1 class='text-4xl mt-10 mb-5'>Welcome to hexastats</h1>
+
+        <hr class='md:w-1/2 mx-auto' />
+
+        <div class='my-6'>
+            <p>A data visualization webapp for League of Legends !</p>
+            <p>Search players from multiple servers and start comparing them !</p>
+        </div>
+    </div>
+
+    <!-- <AddPlayer />
+
+    <AddedPlayers /> -->
+
+    <div class='relative mt-10 h-64 md:h-96 w-64 md:w-96 mx-auto opacity-80 px-10'>
+        <img src='/images/programming.svg' alt='Programming' />
+    </div>
 </div>
