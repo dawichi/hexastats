04ba0af3359502c62c852f4a26f90b8678d55870
David M. Fajardo <davidmf99@gmail.com>
2022-11-07 18:48:08 +0100

    Update dependencies

--- frontend/src/lib/components/logic/ListPlayersHeader.svelte
+++ frontend/src/lib/components/logic/ListPlayersHeader.svelte
@@ -4,6 +4,7 @@
   Lists a compressed list of players in context
 -->
 <script lang="ts">
+    import { page } from '$app/stores'
     import { styles } from '$lib/config'
     import { playersContext } from '$lib/context/players'
     import type { SummonerDto } from '$lib/types'
@@ -19,38 +20,34 @@
         playersContext.update(x => start.concat(end))
         localStorage.setItem('players', JSON.stringify(start.concat(end)))
     }
-
-    const route_exceptions = ['/', '/summoners']
-
-    // if (route_exceptions.includes(router.pathname)) {
-    //     return <></>
-    // }
 </script>
 
-<div class="flex flex-wrap items-center border border-x-0 border-t-0 py-2">
-    <div class="mr-3 flex flex-col items-center justify-center">
-        <span class="px-2">Add more</span>
-        <a href="/">
-            <button
-                title="Add more players"
-                class={`${styles.foreground} ${styles.card} hover:text-white hover:bg-indigo-600 hover:dark:bg-indigo-600 m-1 py-2 px-3 cursor-pointer`}
-            >
-                <i class="bi bi-person-plus-fill" />
-            </button>
-        </a>
-    </div>
-    {#each _players as player, idx}
-        <div class={`${styles.foreground} rounded shadow-sm m-1 p-2 relative border border-zinc-300 dark:border-zinc-600`}>
-            <div class="flex items-end justify-center">
-                <div class="relative h-12 w-12 overflow-hidden rounded">
-                    <Image image={player.image} />
-                </div>
-                <span class="ml-2">{player.alias}</span>
-
-                <button on:click={() => handleDeletePlayer(idx)} class="absolute top-0 right-0 rounded-sm p-1 px-2 hover:bg-red-500">
-                    <i class="bi bi-person-x-fill" />
+{#if $page.url.pathname !== '/' && $page.url.pathname !== '/summoners'}
+    <div class="flex flex-wrap items-center border border-x-0 border-t-0 py-2">
+        <div class="mr-3 flex flex-col items-center justify-center">
+            <span class="px-2">Add more</span>
+            <a href="/">
+                <button
+                    title="Add more players"
+                    class={`${styles.foreground} ${styles.card} hover:text-white hover:bg-indigo-600 hover:dark:bg-indigo-600 m-1 py-2 px-3 cursor-pointer`}
+                >
+                    <i class="bi bi-person-plus-fill" />
                 </button>
-            </div>
+            </a>
         </div>
-    {/each}
-</div>
+        {#each _players as player, idx}
+            <div class={`${styles.foreground} rounded shadow-sm m-1 p-2 relative border border-zinc-300 dark:border-zinc-600`}>
+                <div class="flex items-end justify-center">
+                    <div class="relative h-12 w-12 overflow-hidden rounded">
+                        <Image image={player.image} />
+                    </div>
+                    <span class="ml-2">{player.alias}</span>
+
+                    <button on:click={() => handleDeletePlayer(idx)} class="absolute top-0 right-0 rounded-sm p-1 px-2 hover:bg-red-500">
+                        <i class="bi bi-person-x-fill" />
+                    </button>
+                </div>
+            </div>
+        {/each}
+    </div>
+{/if}
