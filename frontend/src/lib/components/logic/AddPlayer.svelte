873957fcf376f408d0a26146cf8f47462bbd7bb1
David M. Fajardo <51042900+dawichi@users.noreply.github.com>
2023-01-13 13:15:13 +0100

    [ FIX ]: ring style

--- frontend/src/lib/components/logic/AddPlayer.svelte
+++ frontend/src/lib/components/logic/AddPlayer.svelte
@@ -110,7 +110,7 @@
         <input
             placeholder="Summoner name"
             type="text"
-            class="h-12 w-full rounded border-0 bg-white p-2 shadow outline-0 ring-red-400 focus:ring-4 dark:bg-zinc-800"
+            class="h-12 w-full rounded border-0 bg-white p-2 shadow outline-0 ring-indigo-400 focus:ring-4 dark:bg-zinc-800"
             on:keypress={handleKeyPress}
             bind:value={username}
         />
