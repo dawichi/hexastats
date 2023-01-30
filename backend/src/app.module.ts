8adfe0ad3226d17b0331298c2ae7e071ca26f0d0
Rodriagra <81830930+Rodriagra@users.noreply.github.com>
2023-01-30 13:52:41 +0100

    controller>servicio

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -3,10 +3,11 @@ import { LoggerMiddleware } from './common/middlewares/logger.middleware'
 import { SummonersModule } from './modules/summoners/summoners.module'
 import { ConfigModule } from '@nestjs/config'
 import { RiotModule } from './modules/riot/riot.module'
+import { RiotController } from './riot/riot.controller';
 
 @Module({
     imports: [ConfigModule.forRoot(), SummonersModule, RiotModule],
-    controllers: [],
+    controllers: [RiotController],
     providers: [],
 })
 export class AppModule implements NestModule {
