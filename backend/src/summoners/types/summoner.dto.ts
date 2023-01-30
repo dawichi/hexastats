9c4fca86eb12edb051752c13a63c48dd73fc95d5
David M. Fajardo <51042900+dawichi@users.noreply.github.com>
2023-01-30 10:51:45 +0100

    Move types

--- backend/src/summoners/types/summoner.dto.ts
+++
@@ -1,9 +0,0 @@
-export class SummonerDto {
-    id: string
-    accountId: string
-    puuid: string
-    name: string
-    profileIconId: number
-    revisionDate: number
-    summonerLevel: number
-}
