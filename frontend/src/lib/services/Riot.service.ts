13fa634b8f72f29457a31535f69bc4b349e80deb
Alex Paz <91157929+alexxwe@users.noreply.github.com>
2023-02-08 15:56:12 +0100

    champName "fiddlesticks" fix

--- frontend/src/lib/services/Riot.service.ts
+++ frontend/src/lib/services/Riot.service.ts
@@ -3,7 +3,7 @@
  * Riot's assets have different URLs depending on the type.
  * So this service provides a centralized way to get the correct URL for each asset.
  */
- export class RiotService {
+export class RiotService {
     private readonly baseUrl: string = 'http://ddragon.leagueoflegends.com/cdn/'
 
     /**
@@ -11,16 +11,16 @@
      * @param champName The name of the champ
      */
     static champImage(champName: string): string {
-        // TODO: use this one. The opp-static doesn't work for Fiddle
-        // https://raw.communitydragon.org/latest/game/assets/characters/fiddlesticks/hud/fiddlesticks_square.png
-        return `https://opgg-static.akamaized.net/images/lol/champion/${champName}.png`
+        // exception only for fiddlesticks
+        if (champName === 'FiddleSticks') champName = 'Fiddlesticks'
+        return `http://ddragon.leagueoflegends.com/cdn/13.1.1/img/champion/${champName}.png`
     }
 
     /**
      * ## URL to the champ splash art
      * @param champName The name of the champ
      */
-     static champSplash(champName: string): string {
+    static champSplash(champName: string): string {
         return `https://ddragon.leagueoflegends.com/cdn/img/champion/splash/${champName}_0.jpg`
     }
 
@@ -28,13 +28,13 @@
      * ## URL to the position image
      * @param position The position of the champ
      */
-     static teamPositionIcon(position: string): string {
+    static teamPositionIcon(position: string): string {
         const positions: Record<string, string> = {
             TOP: 'position_top.png',
             MIDDLE: 'position_mid.png',
             JUNGLE: 'position_jungle.png',
             BOTTOM: 'position_bottom.png',
-            UTILITY: 'position_support.png'
+            UTILITY: 'position_support.png',
         }
 
         return `https://raw.communitydragon.org/latest/plugins/rcp-fe-lol-career-stats/global/default/${positions[position] ?? positions.MIDDLE}`
