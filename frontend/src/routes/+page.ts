5a8649263045b17bca97e5d4f7dd18dd804534fe
Alex Paz <91157929+alexxwe@users.noreply.github.com>
2023-03-01 13:43:48 +0100

    [FIX]: error cached player

--- frontend/src/routes/+page.ts
+++ frontend/src/routes/+page.ts
@@ -1,13 +1,12 @@
 import { backendUrl } from '$lib/services/Summoner.service'
 import type { CachedNameDto } from '$lib/types'
-import { error } from '@sveltejs/kit'
 
 /** @type {import('./$types').PageLoad} */
 export async function load(): Promise<{ cachedPlayers: CachedNameDto[] }> {
     try {
         const response = await fetch(`${backendUrl}database/print`)
         const data = await response.json()
-        
+
         // keys = ['server:name', 'server:name', ...]
         const keys: string[] = data.keys.map((key: string) => {
             const [server, name] = key.split(':')
@@ -15,11 +14,11 @@ export async function load(): Promise<{ cachedPlayers: CachedNameDto[] }> {
         })
         const promises = data.keys.map((key: string) => {
             const [server, name] = key.split(':')
-            return fetch(`http://localhost:5000/summoners/${server}/${name}/level-image`)
+            return fetch(`${backendUrl}summoners/${server}/${name}/level-image`)
         })
         const responses = await Promise.all(promises)
         const datas = await Promise.all(responses.map(res => res.json()))
-
+        
         return {
             cachedPlayers: [...new Set(keys)].map((key: string) => ({
                 server: key.split(':')[0],
@@ -29,6 +28,9 @@ export async function load(): Promise<{ cachedPlayers: CachedNameDto[] }> {
             })),
         }
     } catch (e: unknown) {
-        throw error(500, 'Server is not up, try in a few minutes.')
+        console.error("Cached Players not working")
+        return {
+            cachedPlayers: [],
+        }
     }
 }
