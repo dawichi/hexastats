6f5bc04fe929c022d7a41d5b36bf6eeea89091e5
Alex Paz <91157929+alexxwe@users.noreply.github.com>
2023-03-01 12:03:16 +0100

    fetch image and level (CachedName)

--- frontend/src/routes/+page.ts
+++ frontend/src/routes/+page.ts
@@ -1,26 +1,31 @@
 import { backendUrl } from '$lib/services/Summoner.service'
+import type { CachedNameDto } from '$lib/types'
 import { error } from '@sveltejs/kit'
 
-type CachedPlayer = {
-    server: string
-    name: string
-}
-
 /** @type {import('./$types').PageLoad} */
-export async function load(): Promise<{ cachedPlayers: CachedPlayer[] }> {
+export async function load(): Promise<{ cachedPlayers: CachedNameDto[] }> {
     try {
         const response = await fetch(`${backendUrl}database/print`)
         const data = await response.json()
-
+        
         // keys = ['server:name', 'server:name', ...]
         const keys: string[] = data.keys.map((key: string) => {
             const [server, name] = key.split(':')
             return `${server}:${decodeURI(name)}`
         })
+        const promises = data.keys.map((key: string) => {
+            const [server, name] = key.split(':')
+            return fetch(`http://localhost:5000/summoners/${server}/${name}/level-image`)
+        })
+        const responses = await Promise.all(promises)
+        const datas = await Promise.all(responses.map(res => res.json()))
+
         return {
             cachedPlayers: [...new Set(keys)].map((key: string) => ({
                 server: key.split(':')[0],
                 name: key.split(':')[1],
+                image: datas[keys.indexOf(key)].image,
+                level: datas[keys.indexOf(key)].level,
             })),
         }
     } catch (e: unknown) {
