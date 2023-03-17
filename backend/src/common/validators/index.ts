bf0690156ee7873c947c501061bf7368c0355f39
David M. Fajardo <51042900+Dawichi@users.noreply.github.com>
2023-03-17 18:58:28 +0100

    Update index.ts

--- backend/src/common/validators/index.ts
+++ backend/src/common/validators/index.ts
@@ -1,5 +1,7 @@
+// Validators used on startup
 export { validateDatabase } from './validateDatabase'
 export { validateEnv } from './validateEnv'
+// Other validators
 export { validateQueueType } from './validateQueueType'
 export { validateGameType } from './validateGameType'
 export { validateTTL } from './validateTTL'
