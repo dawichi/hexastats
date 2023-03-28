c50416f38995088bde4528488ac6b625baa023c8
David M. Fajardo <51042900+dawichi@users.noreply.github.com>
2023-03-28 12:44:37 +0200

    [ FEATURE ]: Delete old types

--- backend/src/common/types/index.ts
+++ backend/src/common/types/index.ts
@@ -1,2 +1,12 @@
-export { Game as GameDto } from './Game.dto'
-export { GameDetail as GameDetailDto } from './GameDetail.dto'
+// summoners/
+export { Game as GameDto } from './summoners/Game.dto'
+export { GameDetail as GameDetailDto } from './summoners/GameDetail.dto'
+export { Mastery as MasteryDto } from './summoners/Mastery.dto'
+export { Rank as RankDto } from './summoners/Rank.dto'
+export { Player as PlayerDto } from './summoners/Player.dto'
+export { RankData as RankDataDto } from './summoners/Player.dto'
+
+// redis/
+export { RedisRecordGames as RedisRecordGamesDto } from './redis/RedisRecord.dto'
+export { RedisRecordMasteries as RedisRecordMasteriesDto } from './redis/RedisRecord.dto'
+export { PrintDatabase as PrintDatabaseDto } from './redis/responses.dto'
