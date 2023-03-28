8cd294acac380e2d85cfea0e2ea87a9eb15a1a2c
David M. Fajardo <51042900+dawichi@users.noreply.github.com>
2023-03-28 12:44:55 +0200

    Delete more types

--- backend/src/types/index.ts
+++
@@ -1,6 +0,0 @@
-// Player DTOs are used to return the data to the client.
-export { Game as GameDto } from './Game.dto'
-export { Team as TeamDto } from './Game.dto'
-export { Mastery as MasteryDto } from './Mastery.dto'
-export { Rank as RankDto } from './Rank.dto'
-export { Player as PlayerDto } from './Player.dto'
