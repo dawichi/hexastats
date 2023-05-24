2de7dbeb12e7a8147068fd392076f91d3968a0e6
Alex Paz <91157929+alexxwe@users.noreply.github.com>
2023-05-24 17:04:04 +0200

    [FIX] : masteries

--- frontend/src/lib/components/common/MasteryRow.svelte
+++ frontend/src/lib/components/common/MasteryRow.svelte
@@ -10,22 +10,27 @@
     import Image from './Image.svelte'
 
     export let masteries: MasteryDto[]
+
+    // Show max 6 masteries
+    if (masteries.length > 6) {
+        masteries.length = 6
+    }
 </script>
 
 <div class="grid w-96 grid-cols-6">
-    {#each [0, 1, 2, 3, 4, 5] as idx}
+    {#each masteries as mastery}
         <div class="p-2 text-center">
-            <span class="whitespace-nowrap">{parse_k_num(masteries[idx].points, 0, true)}</span>
+            <span class="whitespace-nowrap">{parse_k_num(mastery.points, 0, true)}</span>
 
             <div class="flex items-center justify-center">
                 <div class="relative flex h-24 w-16 justify-center">
                     <div class="t-0 r-0 absolute">
                         <div class="relative h-24 w-12 rounded">
-                            {#if [5, 6, 7].includes(masteries[idx].level)}
-                                <Image image={'/images/mastery/mastery_' + masteries[idx].level + '.png'} />
+                            {#if [5, 6, 7].includes(mastery.level)}
+                                <Image image={'/images/mastery/mastery_' + mastery.level + '.png'} />
                             {/if}
 
-                            {#if masteries[idx].chestGranted}
+                            {#if mastery.chestGranted}
                                 <span class="absolute -top-1 -right-3 z-20 h-6 w-6 rounded-full">
                                     <Image image="/images/mastery/chest.png" />
                                 </span>
@@ -35,7 +40,7 @@
 
                     <div class="t-0 r-0 absolute">
                         <div class="relative h-12 w-12 rounded">
-                            <Image image={masteries[idx].image} />
+                            <Image image={mastery.image} />
                         </div>
                     </div>
                 </div>
