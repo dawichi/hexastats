e24dc958e10f087d9388cba946b27d9b68d21c77
David M. Fajardo <51042900+dawichi@users.noreply.github.com>
2023-06-21 13:38:18 +0200

    Pagination redirection

--- frontend/src/lib/components/common/Pagination.svelte
+++ frontend/src/lib/components/common/Pagination.svelte
@@ -1,32 +1,34 @@
-<div class="flex items-center justify-center py-10 lg:px-0 sm:px-6 px-4">
-    <div class="lg:w-3/5 w-full  flex items-center justify-between border-t border-gray-200 dark:border-gray-700">
-        <div class="flex items-center pt-3 text-gray-600 dark:text-gray-200  hover:text-indigo-700 cursor-pointer">
-            <svg width="14" height="8" viewBox="0 0 14 8" fill="none" xmlns="http://www.w3.org/2000/svg">
-                <path d="M1.1665 4H12.8332" stroke="currentColor" stroke-width="1.25" stroke-linecap="round" stroke-linejoin="round"/>
-                <path d="M1.1665 4L4.49984 7.33333" stroke="currentColor" stroke-width="1.25" stroke-linecap="round" stroke-linejoin="round"/>
-                <path d="M1.1665 4.00002L4.49984 0.666687" stroke="currentColor" stroke-width="1.25" stroke-linecap="round" stroke-linejoin="round"/>
-                </svg>
-                <p class="text-sm ml-3 font-medium leading-none ">Previous</p>                    
-        </div>
-        <div class="sm:flex hidden">
-            <p class="text-sm font-medium leading-none cursor-pointer text-gray-600 dark:text-gray-200  hover:text-indigo-700 dark:hover:text-indigo-400 border-t border-transparent hover:border-indigo-400 pt-3 mr-4 px-2">1</p>
-            <p class="text-sm font-medium leading-none cursor-pointer text-gray-600 dark:text-gray-200  hover:text-indigo-700 dark:hover:text-indigo-400 border-t border-transparent hover:border-indigo-400 pt-3 mr-4 px-2">2</p>
-            <p class="text-sm font-medium leading-none cursor-pointer text-gray-600 dark:text-gray-200  hover:text-indigo-700 dark:hover:text-indigo-400 border-t border-transparent hover:border-indigo-400 pt-3 mr-4 px-2">3</p>
-            <p class="text-sm font-medium leading-none cursor-pointer text-indigo-700 dark:text-indigo-400 border-t border-indigo-400 pt-3 mr-4 px-2">4</p>
-            <p class="text-sm font-medium leading-none cursor-pointer text-gray-600 dark:text-gray-200  hover:text-indigo-700 dark:hover:text-indigo-400 border-t border-transparent hover:border-indigo-400 pt-3 mr-4 px-2">5</p>
-            <p class="text-sm font-medium leading-none cursor-pointer text-gray-600 dark:text-gray-200  hover:text-indigo-700 dark:hover:text-indigo-400 border-t border-transparent hover:border-indigo-400 pt-3 mr-4 px-2">6</p>
-            <p class="text-sm font-medium leading-none cursor-pointer text-gray-600 dark:text-gray-200  hover:text-indigo-700 dark:hover:text-indigo-400 border-t border-transparent hover:border-indigo-400 pt-3 mr-4 px-2">7</p>
-            <p class="text-sm font-medium leading-none cursor-pointer text-gray-600 dark:text-gray-200  hover:text-indigo-700 dark:hover:text-indigo-400 border-t border-transparent hover:border-indigo-400 pt-3 mr-4 px-2">8</p>
-        </div>
-        <div class="flex items-center pt-3 text-gray-600 dark:text-gray-200  hover:text-indigo-700 cursor-pointer">
-            <p class="text-sm font-medium leading-none mr-3">Next</p>
-            <svg width="14" height="8" viewBox="0 0 14 8" fill="none" xmlns="http://www.w3.org/2000/svg">
-                <path d="M1.1665 4H12.8332" stroke="currentColor" stroke-width="1.25" stroke-linecap="round" stroke-linejoin="round"/>
-                <path d="M9.5 7.33333L12.8333 4" stroke="currentColor" stroke-width="1.25" stroke-linecap="round" stroke-linejoin="round"/>
-                <path d="M9.5 0.666687L12.8333 4.00002" stroke="currentColor" stroke-width="1.25" stroke-linecap="round" stroke-linejoin="round"/>
+<script lang="ts">
+    import { page } from "$app/stores"
 
-                </svg>
-                
-        </div>
+    const [x, y, server, summoner, page_num] = $page.url.pathname.split("/")
+</script>
+
+<div class="flex items-center justify-between py-4 px-8">
+    {#if Number(page_num) > 1}
+        <a class="bg-indigo-400 rounded px-4 py-1" href="/summoners/{server}/{summoner}/{Number(page_num) - 1}">
+            <i class="bi bi-arrow-left"></i>
+            Previous
+        </a>
+    {/if}
+
+    <!-- <div class="flex items-center justify-center gap-2">
+        {#each Array.from({ length: pages }) as _, idx}
+            <a class="bg-indigo-400 rounded px-4 py-1" href={`/summoners/euw/Dawichii/${idx+1}`}>
+                {idx+1}
+            </a>
+        {/each}
+        <button class="bg-indigo-400 rounded px-4 py-1">
+            <i class="bi bi-arrow-clockwise"></i> Load more
+        </button>
+    </div> -->
+
+    <div>
+
     </div>
+
+    <a class="bg-indigo-400 rounded px-4 py-1" href="/summoners/{server}/{summoner}/{Number(page_num) + 1}">
+        Next
+        <i class="bi bi-arrow-right"></i>
+    </a>
 </div>
