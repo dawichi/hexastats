73fdca346cca9a346181ce8d794a88f8e27f5e61
Alex Paz <91157929+alexxwe@users.noreply.github.com>
2023-06-22 13:22:20 +0200

    Added index classes

--- backend/src/common/types/index.ts
+++ backend/src/common/types/index.ts
@@ -1,10 +1,15 @@
 // summoners/
-export { Game as GameDto } from './summoners/Game.dto'
-export { GameDetail as GameDetailDto } from './summoners/GameDetail.dto'
-export { Mastery as MasteryDto } from './summoners/Mastery.dto'
-export { Rank as RankDto } from './summoners/Rank.dto'
-export { Player as PlayerDto } from './summoners/Player.dto'
-export { RankData as RankDataDto } from './summoners/Player.dto'
+export { Game as GameDto } from './shared/Game.dto'
+export { GameDetail as GameDetailDto } from './shared/GameDetail.dto'
+export { Mastery as MasteryDto } from './shared/Mastery.dto'
+export { Rank as RankDto } from './shared/Rank.dto'
+export { Player as PlayerDto } from './shared/Player.dto'
+export { RankData as RankDataDto } from './shared/Player.dto'
+
+export { Friend as FriendDto } from './shared/Stats.dto'
+export { ChampStats as ChampStatsDto } from './shared/Stats.dto'
+export { PositionStats as PositionStatsDto } from './shared/Stats.dto'
+export { Stats as StatsDto } from './shared/Stats.dto'
 
 // redis/
 export { RedisRecordGames as RedisRecordGamesDto } from './redis/RedisRecord.dto'
