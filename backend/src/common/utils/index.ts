956db442440f3a198668fabb3fdc09e2eb313d64
David M. Fajardo <51042900+dawichi@users.noreply.github.com>
2023-06-22 17:36:13 +0200

    KDA util function in BE

--- backend/src/common/utils/index.ts
+++ backend/src/common/utils/index.ts
@@ -1,3 +1,4 @@
 export { serverRegion } from './serverRegion'
 export { winrate } from './winrate'
+export { kda } from './kda'
 export { perkUrl, runeUrl } from './runeUrl'
---
+++ backend/src/common/utils/kda.ts
@@ -0,0 +1,12 @@
+/**
+ * ## Calculates the kda
+ * (kills + assists) / deaths
+ *
+ * @param kills The number of kills
+ * @param deaths The number of deaths
+ * @param assists The number of assists
+ * @returns The kda value
+ */
+export function kda(kills: number, deaths: number, assists: number) {
+    return parseFloat((deaths ? (kills + assists) / deaths : kills + assists).toFixed(1))
+}
