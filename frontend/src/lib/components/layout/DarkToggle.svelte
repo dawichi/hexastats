9ed9df71ecd106c14961ffd819dda84c28c4a04d
David M. Fajardo <51042900+dawichi@users.noreply.github.com>
2023-07-05 13:45:57 +0200

    DarkMode detection change

--- frontend/src/lib/components/layout/DarkToggle.svelte
+++ frontend/src/lib/components/layout/DarkToggle.svelte
@@ -4,39 +4,42 @@
   A toggle to control the dark mode of the app.
 -->
 <script lang="ts">
-    import { generalContext, type GeneralContextDto } from '$lib/context/general'
+    import { darkModeContext } from '$lib/context/general'
     import { classNames } from '$lib/utils'
     import { onMount } from 'svelte'
 
-    let _general: GeneralContextDto
-    generalContext.subscribe(value => (_general = value))
-
     // Load preferred color scheme
     onMount(() => {
-        const prefersDarkMode = window.matchMedia('(prefers-color-scheme: dark)').matches || localStorage.getItem('theme') === 'dark'
-        if (prefersDarkMode) {
+        // 1. Check if the user has a preference for this website (localStorage)
+        if (localStorage.getItem('theme')) {
+            console.log(`${localStorage.getItem('theme')} mode selected on localStorage`)
+            return localStorage.getItem('theme') === 'dark' ? toggleDarkMode() : null
+        }
+
+        // 2. Check if the user has a preference for the system (OS)
+        if (window.matchMedia('(prefers-color-scheme: dark)').matches) {
+            console.log('dark mode selected on OS')
             toggleDarkMode()
         }
     })
 
     function toggleDarkMode() {
-        const newMode = _general.darkMode ? 'light' : 'dark'
+        const newMode = $darkModeContext ? 'light' : 'dark'
         document.body.dataset.theme = newMode
         localStorage.setItem('theme', newMode)
         document.documentElement.classList.toggle('dark')
-
-        generalContext.update(v => ({ ...v, darkMode: !_general.darkMode }))
+        darkModeContext.set(!$darkModeContext)
     }
 </script>
 
 <button class="h-[34px] w-[58px] cursor-pointer rounded-full bg-orange-50 focus:outline-none dark:bg-zinc-900" on:click={toggleDarkMode}>
     <span
         class={classNames(
-            `${_general.darkMode ? 'translate-x-[25px] bg-zinc-700' : 'translate-x-[2px] bg-orange-200'} pointer-events-none inline-block`,
+            `${$darkModeContext ? 'translate-x-[25px] bg-zinc-700' : 'translate-x-[2px] bg-orange-200'} pointer-events-none inline-block`,
             ' h-[30px] w-[30px] rounded-full transform ring-0 transition ease-in-out duration-700 flex justify-center items-center',
         )}
     >
-        {#if _general.darkMode}
+        {#if $darkModeContext}
             <i class="bi bi-moon-fill text-white animate__animated animate__rotateIn" />
         {:else}
             <i class="bi bi-sun-fill text-black animate__animated animate__rotateInUpRight" />
