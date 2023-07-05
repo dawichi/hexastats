d35b4627360c3e05b1ba4c70c2435cb61eb3b0dc
David M. Fajardo <51042900+dawichi@users.noreply.github.com>
2023-07-05 13:48:19 +0200

    DELETE unused contexts

--- frontend/src/lib/context/cachedPlayers.ts
+++
@@ -1,4 +0,0 @@
-import type { CachedNameDto } from '$lib/types'
-import { writable, type Writable } from 'svelte/store'
-
-export const filteredCachedPlayersContext: Writable<CachedNameDto[]> = writable([])
