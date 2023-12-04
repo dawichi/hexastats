7b4d04d8c76c8889fac973161abe1357b0582c39
David M. Fajardo <51042900+dawichi@users.noreply.github.com>
2023-12-04 14:10:55 +0100

    ServerSchema as source of truth

--- backend/src/common/decorators/params.ts
+++ backend/src/common/decorators/params.ts
@@ -1,12 +1,13 @@
 import { applyDecorators } from '@nestjs/common'
 import { ApiParam } from '@nestjs/swagger'
+import { ServerSchema } from '../schemas'
 
 export function ParamServer() {
     return applyDecorators(
         ApiParam({
             name: 'server',
             description: 'Server to search the player in',
-            enum: ['euw1', 'na1', 'eun1', 'kr', 'jp1', 'br1', 'la1', 'la2', 'oc1', 'tr1', 'ru'],
+            enum: Object.values(ServerSchema.Values),
         }),
     )
 }
