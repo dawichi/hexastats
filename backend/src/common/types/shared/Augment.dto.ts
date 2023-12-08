e3242d4b4c6bf2793a3c8f8d2b53d4167d448706
David M. Fajardo <51042900+dawichi@users.noreply.github.com>
2023-12-08 15:14:28 +0100

    Document AugmentDto

--- backend/src/common/types/shared/Augment.dto.ts
+++ backend/src/common/types/shared/Augment.dto.ts
@@ -1,9 +1,46 @@
+import { ApiProperty } from '@nestjs/swagger'
+
 export class Augment {
+    @ApiProperty({
+        description: 'Description of the augment',
+        example: 'There is a tether between you and your partner that constantly damages enemies inside.',
+    })
     description: string
+
+    @ApiProperty({
+        description: 'Display name of the augment',
+        example: 'Clothesline',
+    })
     displayName: string
+
+    @ApiProperty({
+        description: 'Image of the augment - large',
+        example: 'GoreDrink_large.Arena_Augments_V2.png',
+    })
     iconLarge: string
+
+    @ApiProperty({
+        description: 'Image of the augment - small',
+        example: 'GoreDrink_small.Arena_Augments_V2.png',
+    })
     iconSmall: string
+
+    @ApiProperty({
+        description: 'ID of the augment',
+        example: 110,
+    })
     id: number
+
+    @ApiProperty({
+        description: 'Name of the augment',
+        example: 'Clothesline',
+    })
     name: string
+
+    @ApiProperty({
+        description: 'Tooltip of the augment',
+        example:
+            'There is a tether between you and your partner that deals  magic damage per second.Damage Dealt this Round: Damage Dealt Total: ',
+    })
     tooltip: string
 }
