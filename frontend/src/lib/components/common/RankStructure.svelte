07bd957142d8aa5b456d990a3700969ab218a571
Alex Paz <91157929+alexxwe@users.noreply.github.com>
2023-12-14 16:35:13 +0100

    Arena rank data

--- frontend/src/lib/components/common/RankStructure.svelte
+++ frontend/src/lib/components/common/RankStructure.svelte
@@ -19,8 +19,9 @@
             <ProfileImg image={player.image} level={player.level} />
             <h2 class="mt-2 text-xl">{player.alias && player.alias}</h2>
         </div>
-        <Rank title={'Solo/Duo'} rankdata={player.rank.solo} />
-        <Rank title={'Flex'} rankdata={player.rank.flex} />
+        <Rank title="Solo/Duo" rankdata={player.rank.solo} />
+        <Rank title="Flex" rankdata={player.rank.flex} />
+        <Rank title="Arena" rankdata={player.rank.arena}/>
     </div>
 {:else}
     <!-- Display a placeholder until data is ready -->
@@ -28,11 +29,11 @@
         <div class="flex flex-col items-center justify-center overflow-hidden">
             <!-- ProfileImg -->
             <MockImage />
-            
+
             <!-- Title -->
             <div class="mt-2 h-2.5 w-24 rounded-full bg-zinc-300 dark:bg-zinc-700" />
         </div>
-        
+
         <!-- Rank 1 -->
         <div class="flex flex-col items-center justify-center">
             <div class="my-1 h-2.5 w-16 rounded-full bg-zinc-300 dark:bg-zinc-700" />
@@ -44,7 +45,7 @@
             </div>
             <div class="my-1 h-2.5 w-24 rounded-full bg-zinc-300 dark:bg-zinc-700" />
         </div>
-        
+
         <!-- Rank 2 -->
         <div class="flex flex-col items-center justify-center">
             <div class="my-1 h-2.5 w-16 rounded-full bg-zinc-300 dark:bg-zinc-700" />
